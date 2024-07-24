4ab8c1e38e1f8e0276c422b2404bb45032f008e2
GustavoAssuncao02 <94201266+GustavoAssuncao02@users.noreply.github.com>
2024-07-24 16:51:26 -0300

    Update ScreenShot.py

--- ScreenShot.py
+++ ScreenShot.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-
+ 
 # Configurações do Chrome
 chrome_options = Options()
 chrome_options.add_argument(
